5967b99fbd7e896b8ca0f3c2c0e2ff4040994fac
pextech <mcstain1639@gmail.com>
2021-03-10 22:06:59 +0200

    --amend

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,38 +1,34 @@
-#include "f_pointrs.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of its own main function
- * @argc: line numbers of arguments
- * @argv: array strings
- * Return: 0
+ * main - program that prints the opcodes of its own main function
  *
- * ./main number_of_bytes: 55
- * objdump -d -j.text -M intel main
+ * @argc: count of args
+ * @argv: array of args
+ *
+ * Return: int
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-register int i, n;
-char *ptr = (char *)main;
+int i, bytes;
+char *addr;
 
 if (argc != 2)
 {
-printf("Error\n"), exit(1);
+printf("Error\n");
+exit(1);
 }
-n = atoi(argv[1]);
-
-if (n < 0)
+bytes = atoi(argv[1]);
+if (bytes < 0)
 {
-printf("Error\n"), exit(2);
+printf("Error\n");
+exit(2);
 }
-
-for (i = 0; i < n - 1; i++)
-{
-printf("%02hhx ", ptr[i]);
-}
-
-printf("%02hhx\n", ptr[i]);
+addr = (char *)main;
+for (i = 0; i < bytes - 1; i++)
+printf("%02hhx ", addr[i]);
+printf("%02hhx\n", addr[i]);
 return (0);
 }
